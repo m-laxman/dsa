afef6d319554c05e2562e45db8f12ec719ef58ce
Laxman M <rafadeinol@gmail.com>
2023-08-15 07:02:47 -0400

    Added Add_two_linked_list.cpp

---
+++ Linked List/Add_two_linked_list.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition of linked list:
+ *
+ * class Node {
+ * public:
+ *      int data;
+ *      Node *next;
+ *      Node() {
+ *          this->data = 0;
+ *          this->next = NULL;
+ *      }
+ *      Node(int data) {
+ *          this->data = data;
+ *          this->next = NULL;
+ *      }
+ *      Node (int data, Node *next) {
+ *          this->data = data;
+ *          this->next = next;
+ *      }
+ * };
+ *
+ *************************************************************************/
+
+Node *addTwoNumbers(Node *num1, Node *num2)
+{
+    // Write your code here.
+    Node *head_prev = new Node(-1);
+    Node *temp = head_prev;
+    int carry = 0;
+
+    while (num1 || num2 || carry)
+    {
+        int sum = 0;
+        if (num1 != NULL)
+        {
+            sum += num1->data;
+            num1 = num1->next;
+        }
+        if (num2 != NULL)
+        {
+            sum += num2->data;
+            num2 = num2->next;
+        }
+        sum += carry;
+        carry = sum / 10;
+
+        Node *aux = new Node(sum % 10);
+        temp->next = aux;
+        temp = temp->next;
+    }
+    return head_prev->next;
+}
