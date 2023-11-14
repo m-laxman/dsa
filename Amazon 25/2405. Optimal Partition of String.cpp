d9943ff6700b4b4488a8146f46f75f9964d9ae65
Laxman M <rafadeinol@gmail.com>
2023-11-14 06:57:40 -0500

    Added 2405. Optimal Partition of String.cpp

--- Amazon 25/2405. Optimal Partition of String.cpp
+++ Amazon 25/2405. Optimal Partition of String.cpp
@@ -5,6 +5,8 @@
     Explanation:
     Two possible partitions are ("a","ba","cab","a") and ("ab","a","ca","ba").
     It can be shown that 4 is the minimum number of substrings needed.
+
+    TC - O(n)
 */
 
 int partitionString(string s)
