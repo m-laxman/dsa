98706f5b3ca5ed3388ef05a20af25556f4fbd2ac
Laxman M <rafadeinol@gmail.com>
2023-12-28 06:58:55 -0500

    Added 20. Valid Parentheses.cpp

---
+++ Leetcode 75/20. Valid Parentheses.cpp
@@ -0,0 +1,26 @@
+/* 20. Valid Parentheses
+    Input: s="([]{})"
+    Output: true
+*/
+// O(n), O(n)
+
+bool isValid(string s)
+{
+    stack<char> st;
+    int i = 0;
+    while (i < s.length())
+    {
+        if (s[i] == '(' || s[i] == '[' || s[i] == '{')
+            st.push(s[i]);
+        else
+        {
+            if (st.empty() || (s[i] == ')' && st.top() != '(') || (s[i] == ']' && st.top() != '[') || (s[i] == '}' && st.top() != '{'))
+            {
+                return false;
+            }
+            st.pop();
+        }
+        ++i;
+    }
+    return st.empty();
+}
