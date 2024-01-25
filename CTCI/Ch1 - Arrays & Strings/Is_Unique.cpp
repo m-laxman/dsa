41f44481e195f2081b0e32026c5aee97d565aee6
Laxman M <rafadeinol@gmail.com>
2024-01-25 06:25:27 -0500

    Added Is_Unique.cpp

---
+++ CTCI/Ch1 - Arrays & Strings/Is_Unique.cpp
@@ -0,0 +1,31 @@
+/* Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+LC - 217. Contains Duplicate
+*/
+
+// O(nlogn), O(1)
+bool containsDuplicate(vector<int> &nums)
+{
+    sort(nums.begin(), nums.end());
+    for (int i = 0; i < nums.size() - 1; i++)
+    {
+        if (nums[i] == nums[i + 1])
+            return true;
+    }
+    return false;
+}
+
+//Using bit vector O(n), O(1)
+bool isUnique(string str)
+{
+    int checker = 0;
+    for (int i = 0; i < str.length(); i++)
+    {
+        int val = str[i] - 'a';
+        if ((checker & (1 << val)) > 0)
+        {
+            return false;
+        }
+        checker |= (1 << val);
+    }
+    return true;
+}
