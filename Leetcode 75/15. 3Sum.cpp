473481998e190a2fc935bd38882f5173f8c72a6e
Laxman M <rafadeinol@gmail.com>
2024-04-10 06:55:40 -0400

    Added 15. 3Sum.cpp

---
+++ Leetcode 75/15. 3Sum.cpp
@@ -0,0 +1,39 @@
+/* 15. 3Sum
+    Input: nums = [-1,0,1,2,-1,-4]
+    Output: [[-1,-1,2],[-1,0,1]]
+*/
+// O(n^2), O(1)
+class Solution
+{
+public:
+    vector<vector<int>> threeSum(vector<int> &nums)
+    {
+        vector<vector<int>> res;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size() - 2; i++)
+        {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            int j = i + 1, k = nums.size() - 1;
+            while (j < k)
+            {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum < 0)
+                    j++;
+                else if (sum > 0)
+                    k--;
+                else
+                {
+                    res.push_back({nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
+                    while (j < k && nums[j] == nums[j - 1])
+                        j++;
+                    while (j < k && nums[k] == nums[k + 1])
+                        k--;
+                }
+            }
+        }
+        return res;
+    }
+};
