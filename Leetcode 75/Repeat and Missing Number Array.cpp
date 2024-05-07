1262bc76be6c80d8f8f46e92ae4477786de0219d
Laxman M <rafadeinol@gmail.com>
2024-05-07 05:41:52 -0400

    Added Repeat and Missing Number Array.cpp

---
+++ Leetcode 75/Repeat and Missing Number Array.cpp
@@ -0,0 +1,32 @@
+/*  Repeat and Missing Number Array
+    Input:[3 1 2 5 3]
+    Output:[3, 4]
+    A = 3, B = 4
+*/
+// O(n), O(1)
+vector<int> Solution::repeatedNumber(const vector<int> &A)
+{
+    int n1 = A.size();
+    for (int i = 0; i < n1; i++)
+    {
+        while (A[i] != A[A[i] - 1])
+            swap(const_cast<int &>(A[i]), const_cast<int &>(A[A[i] - 1]));
+    }
+    int temp1, temp2;
+    for (int i = 0; i < n1; i++)
+    {
+        if (A[i] != i + 1)
+        {
+            // cnt++;
+            temp1 = A[i];
+            temp2 = i + 1;
+        }
+    }
+    return {temp1, temp2};
+}
+void swap(int &a, int &b)
+{
+    int t = a;
+    a = b;
+    b = t;
+}
